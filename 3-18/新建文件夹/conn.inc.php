2816383258670eb23de2eb533636de58fe378d2f
softxdlm <996793368@qq.com>
2017-12-14 13:22:39 +0800

    no message

--- 3-18/新建文件夹/conn.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$link = mysql_connect("localhost","root","admin123");
-	mysql_select_db("weixin");
-	mysql_query("set names utf8");
--- 3-18/新建文件夹/move_group.php
+++
@@ -1,7 +0,0 @@
-<?php
-	header("Content-Type:text/html;charset=utf-8");
-	include "common.inc.php";
-	$access_token = get_token();
-	$url = "https://api.weixin.qq.com/cgi-bin/groups/members/update?access_token={$access_token}";
-	$data = '{"openid":"ocoZAuDdpx0a85HXLRkV_DVjP7ow","to_groupid":107}';
-	https_request($url,$data);
